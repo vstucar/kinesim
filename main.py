6a158031bcd8dca4a97e9e9139cb124a0eb2a9d5
Markov Alexey <markovalex95@gmail.com>
2019-03-28 02:15:58 +0300

    Add core of the Entity Component System

---
+++ main.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+import pygame
+from pygame.locals import *
+import os
+
+
+class Engine(object):
+    """
+    Holds all objects and control everything
+    """
+    def __init__(self):
+        self.__entities = []
+        self.__components = []
+        self.__systems = []
+
+    def update(self):
+        for system in self.__systems:
+            system.update()
+
+    def add_entity(self, entity):
+        if entity not in self.__entities:
+            self.__entities.append(entity)
+        else:
+            raise RuntimeError('Entity already added to the Engine')
+
+    def remove_entity(self, entity):
+        if entity in self.__entities:
+            self.__entities.remove(entity)
+        else:
+            raise RuntimeError('Entity not in the Engine')
+
+    def get_entities_with_components(self, components_list):
+        return [entity for entity in self.__entities if
+                all((entity.has_component_class(comp) for comp in components_list))]
+
+    def add_system(self, system):
+        if system not in self.__systems:
+            self.__systems.append(system)
+        else:
+            raise RuntimeError('System already added to the Engine')
+
+
+class BaseComponent(object):
+    """
+    Base class for components, allows access to it's parent entity
+    """
+    def __init__(self, parent):
+        self.__parent = parent
+
+    @property
+    def parent(self):
+        return self.__parent
+
+
+class EngineChild(object):
+    """
+    Base class for something belows to Engine
+    """
+    def __init__(self):
+        self.__engine = None
+
+    @property
+    def engine(self):
+        return self.__engine
+
+    @engine.setter
+    def engine(self, engine):
+        if self.__engine is None:
+            self.__engine = engine
+        else:
+            raise RuntimeError('Engine already set')
+
+
+class Entity(EngineChild):
+    """
+    Entity is a container of the components.
+    This class shouldn't be inherited
+    """
+    def __init__(self, name):
+        super(self.__class__, self).__init__()
+        self.__name = name
+        self.__components = {}
+
+    @property
+    def name(self):
+        return self.__name
+
+    def add_component(self, component):
+        if component.parent is not None:
+            raise RuntimeError('Component can belong only to one entity')
+
+        if component.__class__ not in self.__components:
+            self.__components[component.__class__] = [component]
+        else:
+            clist = self.__components[component.__class__]
+            if component not in clist:
+                component.parent = self
+                clist.append(component)
+                # TODO: Add component to the engine
+            else:
+                raise RuntimeError('Component already added')
+
+    def remove_component(self, component):
+        for comp_class in self.__components:
+            if component in self.__components[comp_class]:
+                self.__components[comp_class].remove(component)
+                # TODO: Remove component from engine
+                return
+        raise RuntimeError('Component not in entity')
+
+    def get_components_by_class(self, component_class):
+        return self.__components[component_class]
+
+    def has_component_class(self, component_class):
+        return len(self.get_components_by_class(component_class)) > 0
+
+
+class BaseSystem(EngineChild):
+    """
+    Systems performs actions
+    """
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        pass
+
+    def update(self):
+        pass
